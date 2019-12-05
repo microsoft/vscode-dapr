93a9d5301f4dfeb3ffe18bd298553dcb84734962
Phillip Hoff <phillip@orst.edu>
2019-12-04 17:16:15 -0800

    Sketch killing daprd process.

--- src/tasks/daprdDownTaskProvider.ts
+++ src/tasks/daprdDownTaskProvider.ts
@@ -1,17 +1,29 @@
-import * as cp from 'child_process';
+import * as psList from 'ps-list';
 import * as vscode from 'vscode';
 import CustomExecutionTaskProvider from "./customExecutionTaskProvider";
-import { Process } from '../util/process';
 
-export type CommandTaskSpawnCallback = (command: string, options?: cp.SpawnOptions) => Promise<void>;
-export type CommandTaskProviderCallback = (definition: vscode.TaskDefinition, callback: CommandTaskSpawnCallback, token?: vscode.CancellationToken) => Promise<void>;
+interface DaprdTaskDefinition extends vscode.TaskDefinition {
+    appId?: string;
+}
 
 export default class DaprdDownTaskProvider extends CustomExecutionTaskProvider {
     constructor() {
         super(
-            (definition, writer, token) => {
+            async (definition: DaprdTaskDefinition, writer, token) => {
+                if (definition.appId === undefined) {
+                    throw new Error('The \'appId\' property must be set.');
+                }
+
+                const processes = await psList();
+                const daprdProcesses = processes.filter(p => p.name === 'daprd');
+
+                const argumentPattern = `--dapr-id ${definition.appId}`;
+
+                const appProcesses = daprdProcesses.filter(p => p.cmd && p.cmd.indexOf(argumentPattern) >= 0);
+
+                appProcesses.forEach(p => process.kill(p.pid, 'SIGKILL'));
+
                 writer.writeLine('Shutting down daprd...');
-                return Promise.resolve();
             },
             /* isBackgroundTask: */ false,
             /* problemMatchers: */ []);
