06ca13e154e6f27ef3475d3828658fb9ab88819b
Phillip Hoff <phillip@orst.edu>
2020-01-13 15:57:58 -0800

    Scaffold scaffold Dapr tasks command.

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,13 @@ import * as vscode from 'vscode';
 import DaprCommandTaskProvider from './tasks/daprCommandTaskProvider';
 import DaprdCommandTaskProvider from './tasks/daprdCommandTaskProvider';
 import DaprdDownTaskProvider from './tasks/daprdDownTaskProvider';
+import scaffoldDaprTasks from './commands/scaffoldDaprTasks';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-dapr.scaffoldDaprTasks', scaffoldDaprTasks));
+
 	context.subscriptions.push(vscode.tasks.registerTaskProvider("dapr", new DaprCommandTaskProvider()));
 	context.subscriptions.push(vscode.tasks.registerTaskProvider("daprd", new DaprdCommandTaskProvider()));
 	context.subscriptions.push(vscode.tasks.registerTaskProvider("daprd-down", new DaprdDownTaskProvider()));
