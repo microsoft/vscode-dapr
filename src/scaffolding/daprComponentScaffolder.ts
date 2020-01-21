51b51ee99a87f8de9375345e8b97b7a5aa03c936
Phillip Hoff <phillip@orst.edu>
2020-01-21 15:54:25 -0800

    Account for DAPR_NETWORK Redis host default.

--- src/scaffolding/daprComponentScaffolder.ts
+++ src/scaffolding/daprComponentScaffolder.ts
@@ -1,10 +1,12 @@
 import * as fse from 'fs-extra';
 import * as path from 'path';
+import * as process from 'process';
 import scaffoldTemplate from './templateScaffolder';
 
 export async function scaffoldRedisComponent(name: string, folderPath: string, fileName: string, redisHost?: string): Promise<void> {
-    // TODO: Use "redis" host name when a non-default network is used (e.g. DAPR_NETWORK is set).
-    const content = await scaffoldTemplate(name, { redisHost: redisHost ?? 'localhost' });
+    // NOTE: If DAPR_NETWORK is set, Dapr is running within a multi-container context and Redis is a separate container,
+    //       so use 'redis' rather than 'localhost'. 
+    const content = await scaffoldTemplate(name, { redisHost: redisHost ?? (process.env.DAPR_NETWORK ? 'redis' : 'localhost') });
 
     await fse.writeFile(path.join(folderPath, fileName), content, 'utf8');
 }
