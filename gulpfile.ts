825ffcff3168439135004f6082b3f0d44efcae44
Phillip Hoff <phillip@orst.edu>
2020-02-14 14:26:53 -0800

    Add sourcemap generation.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,17 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as del from 'del';
 import * as gulp from 'gulp';
+import * as sourcemaps from 'gulp-sourcemaps';
 import * as ts from 'gulp-typescript';
 
-function buildTask(): NodeJS.ReadWriteStream {
-    const tsProject = ts.createProject('./tsconfig.json');
+const tsProject = ts.createProject('./tsconfig.json');
 
+function getOutDir(): string {
     if (!tsProject.options.outDir) {
-        throw new Error('No outDir in tsconfig.json');
+        throw new Error('outDir is not defined in tsconfig.json.');
     }
 
-    return tsProject.src()
-        .pipe(tsProject()).js
-        .pipe(gulp.dest(tsProject.options.outDir));
+    return tsProject.options.outDir;
 }
+
+function cleanTask(): Promise<string[]> {
+    return del([getOutDir()]);
+}
+
+function compileTask(): NodeJS.ReadWriteStream {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: './' }))
+        .pipe(gulp.dest(getOutDir));
+}
+
+const buildTask = gulp.series(cleanTask, compileTask);
 
 gulp.task('build', buildTask);
 
