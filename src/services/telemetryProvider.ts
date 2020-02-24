3d4a98e409d8066b66be8baa046badbb3cca8784
Phillip Hoff <phillip@orst.edu>
2020-02-23 21:57:53 -0800

    Add source metadata for context commands.

--- src/services/telemetryProvider.ts
+++ src/services/telemetryProvider.ts
@@ -1,13 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { callWithTelemetryAndErrorHandling, IActionContext, registerCommand } from "vscode-azureextensionui";
+import { callWithTelemetryAndErrorHandling, IActionContext, registerCommand, TelemetryProperties } from "vscode-azureextensionui";
+import TreeNode from "../views/applications/treeNode";
+
+interface ContextCommandTelemetryProperties extends TelemetryProperties {
+    source: 'context' | 'palette';
+}
 
 export interface TelemetryProvider {
     callWithTelemetry<T>(eventName: string, action: (context: IActionContext) => Promise<T>): Promise<T | undefined>;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     registerCommandWithTelemetry(commandId: string, callback: (context: IActionContext, ...args: any[]) => unknown): void;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    registerContextCommandWithTelemetry<T extends TreeNode>(commandId: string, callback: (context: IActionContext, node: T | undefined, ...args: any[]) => unknown): void;
 }
 
 export default class AzureTelemetryProvider implements TelemetryProvider {
@@ -18,5 +26,18 @@ export default class AzureTelemetryProvider implements TelemetryProvider {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     registerCommandWithTelemetry(commandId: string, callback: (context: IActionContext, ...args: any[]) => unknown): void {
         registerCommand(commandId, callback);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    registerContextCommandWithTelemetry<T extends TreeNode>(commandId: string, callback: (context: IActionContext, node: T | undefined, ...args: any[]) => unknown): void {
+        this.registerCommandWithTelemetry(
+            commandId,
+            (context, node, ...args) => {
+                const properties = context.telemetry.properties as ContextCommandTelemetryProperties;
+
+                properties.source = node ? 'context' : 'palette';
+
+                return callback(context, node, ...args);
+            });
     }
 }
