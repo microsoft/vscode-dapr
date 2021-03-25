4a28d92a2cfe0792a367afedc00ada7ec255836b
Phillip Hoff <phillip@orst.edu>
2021-03-25 13:14:54 -0700

    Add settings for explicit Dapr binary paths (#149)
    * Update version and dependencies.
    
    * Define configuration setting for daprd path.
    
    * Sketch settings provider.
    
    * Add configuration setting for dapr path.
    
    * Tweak configuration setting names.
    
    * Add file header.
    
    * Use correct dapr configuration setting.

---
+++ src/services/settingsProvider.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as vscode from 'vscode';
+
+export interface SettingsProvider {
+    readonly daprPath: string;
+    readonly daprdPath: string;
+}
+
+export default class VsCodeSettingsProvider implements SettingsProvider {
+    get daprPath(): string {
+        return this.getConfigurationValue('daprPath') || 'dapr';
+    }
+
+    get daprdPath(): string {
+        return this.getConfigurationValue('daprdPath') || 'daprd';
+    }
+
+    private getConfigurationValue(name: string): string | undefined {
+        const configuration = vscode.workspace.getConfiguration('dapr.paths');
+
+        return configuration.get(name);
+    }
+}
