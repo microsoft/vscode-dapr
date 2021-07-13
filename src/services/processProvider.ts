93e68b4f07c362819976b5861631ea67f49e8645
sk593 <shruthikumar2401@gmail.com>
2021-07-13 13:05:14 -0500

    minor changes to process provider logic

--- src/services/processProvider.ts
+++ src/services/processProvider.ts
@@ -1,11 +1,7 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
 import * as psList from 'ps-list';
 import * as os from 'os';
-import * as which from 'which'
-import * as fs from 'fs'
 import { Process } from '../util/process';
+import * as fs from 'fs'
 
 export interface ProcessInfo {
     cmd: string;
@@ -18,6 +14,7 @@ export interface ProcessProvider {
 }
 
 export class UnixProcessProvider implements ProcessProvider {
+
     async listProcesses(name: string, daprdPath: string): Promise<ProcessInfo[]> {
         const processes = await psList();
         return processes
@@ -26,12 +23,9 @@ export class UnixProcessProvider implements ProcessProvider {
     }
 
     hasDaprdPath(process: psList.ProcessDescriptor, daprdPath: string): boolean | undefined {
-        const daprd = which.sync('daprd', {nothrow: true});
-        if(daprd !== null) { //check if daprd is in PATH env
-            return process.cmd?.startsWith(daprd); 
-        } else if (daprd === null && daprdPath !== "daprd") { //if not in PATH, check if config path provided
-            return process.cmd?.startsWith(daprdPath)
-        } else { //if not in PATH and no config path, check if filepath is daprd executable
+        if (daprdPath !== "daprd") { //check if config path provided
+            return process.cmd?.startsWith(daprdPath.concat(" "))
+        } else { //if no config path, check if filepath is executable
             const daprdEndPoint = "/daprd ";
             const endpoint = process.cmd?.indexOf(daprdEndPoint);
             if(endpoint !== undefined && endpoint !== -1) {
@@ -39,9 +33,9 @@ export class UnixProcessProvider implements ProcessProvider {
                 if(executable !== undefined) {
                     try {
                         fs.accessSync(executable, fs.constants.X_OK);
-                        return process.cmd?.startsWith(executable);
+                        return process.cmd?.startsWith(executable.concat(" "));
                     } catch(ex) {
-                        return;
+                        return false;
                     }
                 }
             }
