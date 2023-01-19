9d1b098622228a48c4665bd9c9c7c64798e82671
Phillip Hoff <phillip@orst.edu>
2023-01-19 14:09:03 -0800

    Bring forward `dapr` arguments.

--- src/tasks/daprCommandTaskProvider.ts
+++ src/tasks/daprCommandTaskProvider.ts
@@ -9,6 +9,10 @@ import { IActionContext } from '@microsoft/vscode-azext-utils';
 import { DaprInstallationManager } from '../services/daprInstallationManager';
 
 export interface DaprTaskDefinition extends TaskDefinition {
+    appHealthCheckPath?: string;
+    appHealthProbeInterval?: number;
+    appHealthProbeTimeout?: number;
+    appHealthThreshold?: number;
     appId?: string;
     appMaxConcurrency?: number;
     appPort?: number;
@@ -19,13 +23,19 @@ export interface DaprTaskDefinition extends TaskDefinition {
     componentsPath?: string;
     config?: string;
     cwd?: string;
+    enableApiLogging?: boolean;
+    enableHealthCheck?: boolean;
     enableProfiling?: boolean;
     grpcPort?: number;
+    httpMaxRequestSize?: number;
     httpPort?: number;
-    logLevel?: 'debug' | 'info' | 'warning' | 'error' | 'fatal' | 'panic';
+    httpReadBufferSize?: number;
+    logLevel?: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'panic';
+    metricsPort?: number;
     placementHostAddress?: string;
     profilePort?: number;
     type: 'dapr';
+    unixDomainSocket?: string;
 }
 
 export default class DaprCommandTaskProvider extends CommandTaskProvider {
@@ -42,6 +52,10 @@ export default class DaprCommandTaskProvider extends CommandTaskProvider {
                         const command =
                             CommandLineBuilder
                                 .create(daprPathProvider(), 'run')
+                                .withNamedArg('--app-health-check-path', daprDefinition.appHealthCheckPath)
+                                .withNamedArg('--app-health-probe-interval', daprDefinition.appHealthProbeInterval)
+                                .withNamedArg('--app-health-probe-timeout', daprDefinition.appHealthProbeTimeout)
+                                .withNamedArg('--app-health-threshold', daprDefinition.appHealthThreshold)
                                 .withNamedArg('--app-id', daprDefinition.appId)
                                 .withNamedArg('--app-max-concurrency', daprDefinition.appMaxConcurrency)
                                 .withNamedArg('--app-port', daprDefinition.appPort)
@@ -50,11 +64,17 @@ export default class DaprCommandTaskProvider extends CommandTaskProvider {
                                 .withNamedArg('--components-path', daprDefinition.componentsPath)
                                 .withNamedArg('--config', daprDefinition.config)
                                 .withNamedArg('--dapr-grpc-port', daprDefinition.grpcPort)
+                                .withNamedArg('--dapr-http-max-request-size', daprDefinition.httpMaxRequestSize)
                                 .withNamedArg('--dapr-http-port', daprDefinition.httpPort)
+                                .withNamedArg('--dapr-http-read-buffer-size', daprDefinition.httpReadBufferSize)
+                                .withNamedArg('--enable-api-logging', daprDefinition.enableApiLogging, { assignValue: true })
+                                .withNamedArg('--enable-app-health-check', daprDefinition.enableHealthCheck, { assignValue: true })
                                 .withNamedArg('--enable-profiling', daprDefinition.enableProfiling, { assignValue: true })
                                 .withNamedArg('--log-level', daprDefinition.logLevel)
+                                .withNamedArg('--metrics-port', daprDefinition.metricsPort)
                                 .withNamedArg('--placement-host-address', daprDefinition.placementHostAddress)
                                 .withNamedArg('--profile-port', daprDefinition.profilePort)
+                                .withNamedArg('--unix-domain-socket', daprDefinition.unixDomainSocket)
                                 .withArgs(daprDefinition.args)
                                 .withArgs(daprDefinition.command)
                                 .build();
