860ffe9efe3b1b178c1b58d6e5046c2c2e32f56b
Phillip Hoff <phillip@orst.edu>
2023-02-08 21:22:03 -0800

    Implement stop command with confirmation prompt.

--- src/commands/applications/stopRun.ts
+++ src/commands/applications/stopRun.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 import * as nls from 'vscode-nls';
+import * as vscode from 'vscode';
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { DaprRunNode } from "../../views/applications/daprRunNode";
 import { getLocalizationPathForFile } from '../../util/localization';
@@ -10,7 +11,19 @@ import { DaprCliClient } from '../../services/daprCliClient';
 const localize = nls.loadMessageBundle(getLocalizationPathForFile(__filename));
 
 async function stopRun(context: IActionContext, label: string, runTemplatePath: string, daprCliClient: DaprCliClient): Promise<void> {
-    await Promise.resolve();
+    const stopItem: vscode.MessageItem = { title: localize('commands.applications.stopRun.stopItemTitle', 'Stop Run') };
+    
+    const selectedItem = await context.ui.showWarningMessage(
+        localize('commands.applications.stopRun.confirmationMessage', 'Stop the Dapr run \'{0}\'?', label),
+        {
+            detail: localize('commands.applications.stopRun.detailMessage', 'All applications associated with the run will be stopped.'),
+            modal: true
+        },
+        stopItem);
+
+    if (selectedItem === stopItem) {
+        await daprCliClient.stopRun(runTemplatePath);
+    }
 }
 
 const createStopRunCommand = (daprCliClient: DaprCliClient) => (context: IActionContext, node: DaprRunNode | undefined): Promise<void> => {
