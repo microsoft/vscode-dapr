1f572272fdcd6878eb7526e5aa322e8de0cd68e3
MregXN <46479059+MregXN@users.noreply.github.com>
2023-08-12 07:06:22 +0800

    Infer `dapr.yaml` when omitted from configuration (#300)
    * Infer dapr.yaml when omitted from configuration
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * loop configuration
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * modify some typos
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * add file validation
    
    * replace hasOwnProperty method without trigger error
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * async version of file exists check
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * add function of creating command line builder
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * fix typos
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    * remove --runfile argument
    
    Signed-off-by: MregXN <mregxn@gmail.com>
    
    ---------
    
    Signed-off-by: MregXN <mregxn@gmail.com>

--- src/tasks/daprCommandTaskProvider.ts
+++ src/tasks/daprCommandTaskProvider.ts
@@ -7,6 +7,11 @@ import { TaskDefinition } from './taskDefinition';
 import { TelemetryProvider } from '../services/telemetryProvider';
 import { IActionContext } from '@microsoft/vscode-azext-utils';
 import { DaprInstallationManager } from '../services/daprInstallationManager';
+import * as fs from 'fs/promises';
+import path from 'path';
+import * as vscode from 'vscode';
+import * as nls from 'vscode-nls';
+import { getLocalizationPathForFile } from '../util/localization';
 
 export interface DaprTaskDefinition extends TaskDefinition {
     appChannelAddress?: string;
@@ -44,6 +49,8 @@ export interface DaprTaskDefinition extends TaskDefinition {
     unixDomainSocket?: string;
 }
 
+const localize = nls.loadMessageBundle(getLocalizationPathForFile(__filename));
+
 export default class DaprCommandTaskProvider extends CommandTaskProvider {
     constructor(daprInstallationManager: DaprInstallationManager, daprPathProvider: () => string, telemetryProvider: TelemetryProvider) {
         super(
@@ -51,50 +58,86 @@ export default class DaprCommandTaskProvider extends CommandTaskProvider {
                 return telemetryProvider.callWithTelemetry(
                     'vscode-dapr.tasks.dapr',
                     async (context: IActionContext) => {
+
                         await daprInstallationManager.ensureInitialized(context.errorHandling);
 
                         const daprDefinition = definition as DaprTaskDefinition;
 
-                        const command =
-                            CommandLineBuilder
-                                .create(daprPathProvider(), 'run')
-                                .withNamedArg('--app-channel-address', daprDefinition.appChannelAddress)
-                                .withNamedArg('--app-health-check-path', daprDefinition.appHealthCheckPath)
-                                .withNamedArg('--app-health-probe-interval', daprDefinition.appHealthProbeInterval)
-                                .withNamedArg('--app-health-probe-timeout', daprDefinition.appHealthProbeTimeout)
-                                .withNamedArg('--app-health-threshold', daprDefinition.appHealthThreshold)
-                                .withNamedArg('--app-id', daprDefinition.appId)
-                                .withNamedArg('--app-max-concurrency', daprDefinition.appMaxConcurrency)
-                                .withNamedArg('--app-port', daprDefinition.appPort)
-                                .withNamedArg('--app-protocol', daprDefinition.appProtocol)
-                                .withNamedArg('--app-ssl', daprDefinition.appSsl, { assignValue: true })
-                                .withNamedArg('--components-path', daprDefinition.componentsPath)
-                                .withNamedArg('--config', daprDefinition.config)
-                                .withNamedArg('--dapr-grpc-port', daprDefinition.grpcPort)
-                                .withNamedArg('--dapr-http-max-request-size', daprDefinition.httpMaxRequestSize)
-                                .withNamedArg('--dapr-http-port', daprDefinition.httpPort)
-                                .withNamedArg('--dapr-http-read-buffer-size', daprDefinition.httpReadBufferSize)
-                                .withNamedArg('--dapr-internal-grpc-port', daprDefinition.internalGrpcPort)
-                                .withNamedArg('--dapr-listen-addresses', daprDefinition.listenAddresses)
-                                .withNamedArg('--enable-api-logging', daprDefinition.enableApiLogging, { assignValue: true })
-                                .withNamedArg('--enable-app-health-check', daprDefinition.enableHealthCheck, { assignValue: true })
-                                .withNamedArg('--enable-profiling', daprDefinition.enableProfiling, { assignValue: true })
-                                .withNamedArg('--log-level', daprDefinition.logLevel)
-                                .withNamedArg('--metrics-port', daprDefinition.metricsPort)
-                                .withNamedArg('--placement-host-address', daprDefinition.placementHostAddress)
-                                .withNamedArg('--profile-port', daprDefinition.profilePort)
-                                .withNamedArg('--resources-path', daprDefinition.resourcesPath)
-                                .withArrayArgs('--resources-path', daprDefinition.resourcesPaths)
-                                .withNamedArg('--run-file', daprDefinition.runFile)
-                                .withNamedArg('--unix-domain-socket', daprDefinition.unixDomainSocket)
-                                .withArgs(daprDefinition.args)
-                                .withArgs(daprDefinition.command)
-                                .build();
-                        
-                        return callback(command, { cwd: definition.cwd });
+                        // infer dapr.yaml when omitted from configuration
+                        for (const def in daprDefinition) {
+                            // skip daprDefinition.type as it will be set by default
+                            // if any property being set means do not need to infer dapr.yaml
+                            if (Object.prototype.hasOwnProperty.call(daprDefinition, def) && def !== "type" && def !== undefined) {
+                                const command = createCommandLineBuilder(daprPathProvider, daprDefinition).build();
+                                return callback(command, { cwd: definition.cwd });
+                            }
+                        }
+
+                        // when all properties are undefined the command "dapr run --run-file ./dapr.yaml" will be excuted.
+                        const folder = vscode.workspace.workspaceFolders?.[0];
+
+                        if (!folder) {
+                            context.errorHandling.suppressReportIssue = true;
+                            throw new Error(localize('tasks.daprCommandTaskProvider.noFolderOrWorkspace', 'Open a folder or workspace.'));
+                        }
+
+                        const runFilePath = path.join(folder.uri.fsPath, 'dapr.yaml');
+
+                        await checkFileExists(runFilePath).then((fileExists) => {
+                            if (fileExists) {
+                                const command = createCommandLineBuilder(daprPathProvider, { type: 'dapr', runFile: runFilePath }).build();
+                                return callback(command, { cwd: definition.cwd });
+                            } else {
+                                throw new Error(localize('tasks.daprCommandTaskProvider.noRunFile', 'there is no dapr.yaml in this folder or workspace.'));
+                            }
+                        });
                     });
             },
             /* isBackgroundTask: */ true,
-            /* problemMatchers: */ ['$dapr']);
+            /* problemMatchers: */['$dapr']);
     }
+}
+
+async function checkFileExists(filePath: string): Promise<boolean> {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+
+function createCommandLineBuilder(daprPathProvider: () => string, daprDefinition: DaprTaskDefinition) {
+    const commandLineBuilder =
+        CommandLineBuilder
+            .create(daprPathProvider(), 'run')
+            .withNamedArg('--app-health-check-path', daprDefinition.appHealthCheckPath)
+            .withNamedArg('--app-health-probe-interval', daprDefinition.appHealthProbeInterval)
+            .withNamedArg('--app-health-probe-timeout', daprDefinition.appHealthProbeTimeout)
+            .withNamedArg('--app-health-threshold', daprDefinition.appHealthThreshold)
+            .withNamedArg('--app-id', daprDefinition.appId)
+            .withNamedArg('--app-max-concurrency', daprDefinition.appMaxConcurrency)
+            .withNamedArg('--app-port', daprDefinition.appPort)
+            .withNamedArg('--app-protocol', daprDefinition.appProtocol)
+            .withNamedArg('--app-ssl', daprDefinition.appSsl, { assignValue: true })
+            .withNamedArg('--components-path', daprDefinition.componentsPath)
+            .withNamedArg('--config', daprDefinition.config)
+            .withNamedArg('--dapr-grpc-port', daprDefinition.grpcPort)
+            .withNamedArg('--dapr-http-max-request-size', daprDefinition.httpMaxRequestSize)
+            .withNamedArg('--dapr-http-port', daprDefinition.httpPort)
+            .withNamedArg('--dapr-http-read-buffer-size', daprDefinition.httpReadBufferSize)
+            .withNamedArg('--enable-api-logging', daprDefinition.enableApiLogging, { assignValue: true })
+            .withNamedArg('--enable-app-health-check', daprDefinition.enableHealthCheck, { assignValue: true })
+            .withNamedArg('--enable-profiling', daprDefinition.enableProfiling, { assignValue: true })
+            .withNamedArg('--log-level', daprDefinition.logLevel)
+            .withNamedArg('--metrics-port', daprDefinition.metricsPort)
+            .withNamedArg('--placement-host-address', daprDefinition.placementHostAddress)
+            .withNamedArg('--profile-port', daprDefinition.profilePort)
+            .withNamedArg('--run-file', daprDefinition.runFile)
+            .withNamedArg('--unix-domain-socket', daprDefinition.unixDomainSocket)
+            .withArgs(daprDefinition.args)
+            .withArgs(daprDefinition.command)
+
+    return commandLineBuilder;
 }
